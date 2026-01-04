0117b17dbc77285d2487e510c3e057d39d217553
Paul <pavel.glugxix@gmail.com>
2026-01-04 11:35:21 +0300

    chore(test): normalize Jest configuration

--- jest.config.ts
+++ jest.config.ts
@@ -1,16 +1,16 @@
 import type { Config } from '@jest/types';
 
 const config: Config.InitialOptions = {
-    preset: 'ts-jest',
-    testEnvironment: 'jsdom',
-    transform: {
-        '^.+\\.tsx?$': 'ts-jest',
-    },
-    moduleNameMapper: {
-        '^@src/(.*)$': '<rootDir>/src/$1',
-        '^@test/(.*)$': '<rootDir>/test/$1',
-    },
-    testPathIgnorePatterns: ['/node_modules/'],
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '^@src/(.*)$': '<rootDir>/src/$1',
+    '^@test/(.*)$': '<rootDir>/test/$1',
+  },
+  testPathIgnorePatterns: ['/node_modules/'],
 };
 
 export default config;
