2399ae37b769e20369ecc924187a609bee458058
Paul <pavel.glugxix@gmail.com>
2026-01-06 12:52:00 +0300

    refactor(domain): rename deepmerge policy for clarity

--- src/domain/policies/merge.policy.ts
+++ src/domain/policies/merge.policy.ts
@@ -1,20 +1,22 @@
 import { mergeWith } from 'lodash';
 import { typedArrayPolicy } from '@src/domain/policies/typed-array.policy';
 
-export const deepMerge = <T, S>(target: T, source: S): T & S => {
-  return mergeWith({}, target, source, (objValue, srcValue) => {
-    if (Array.isArray(objValue) && Array.isArray(srcValue))
-      return srcValue;
+export const mergePolicy = {
+  deepMerge: <T, S>(target: T, source: S): T & S => {
+    return mergeWith({}, target, source, (objValue, srcValue) => {
+      if (Array.isArray(objValue) && Array.isArray(srcValue))
+        return srcValue;
 
-    else if (objValue instanceof Set && srcValue instanceof Set)
-      return srcValue;
+      else if (objValue instanceof Set && srcValue instanceof Set)
+        return srcValue;
 
-    else if (objValue instanceof Map && srcValue instanceof Map)
-      return srcValue;
+      else if (objValue instanceof Map && srcValue instanceof Map)
+        return srcValue;
 
-    else if (typedArrayPolicy.isTypedArray(objValue) && typedArrayPolicy.isTypedArray(srcValue))
-      return srcValue;
+      else if (typedArrayPolicy.isTypedArray(objValue) && typedArrayPolicy.isTypedArray(srcValue))
+        return srcValue;
 
-    return undefined; // default merge
-  });
-}
+      return undefined; // default merge
+    });
+  }
+};
