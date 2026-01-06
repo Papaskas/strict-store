b9b2cd29bcea2325b4c78ab8dd29ba9a402a877f
Paul <pavel.glugxix@gmail.com>
2026-01-06 12:26:24 +0300

    feat(policy): add NonEmptyTuple type guard

---
+++ src/domain/policies/tuple.policy.ts
@@ -0,0 +1,7 @@
+import { NonEmptyTuple } from 'type-fest';
+
+export const tuplePolicy = {
+  isNonEmptyTuple: <T>(value: unknown): value is NonEmptyTuple<T> => {
+    return Array.isArray(value) && value.length > 0;
+  }
+};
