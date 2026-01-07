448a3c83deb7be8986cff81f61f9994174b7ed6d
Paul <pavel.glugxix@gmail.com>
2026-01-07 13:27:42 +0300

    chore(strict-json): remove unused serializer composition module

--- src/modules/strict-json/infrastructure/serialization/serialization.ts
+++
@@ -1,8 +0,0 @@
-import { StrictJsonSerializer } from '@strict-json/strict-json.service';
-import { TypedArrayRegistry } from '@strict-json/infrastructure/typed-array/typed-array.registery';
-import { ComplexTypeCodec } from '@strict-json/infrastructure/serialization/complex-type.codec';
-
-const typedArrayRegistry = new TypedArrayRegistry();
-const codec = new ComplexTypeCodec(typedArrayRegistry);
-
-export const strictJson = new StrictJsonSerializer(codec);
