64e1cec52f7b05d6dd3549318a94c6b83728d4f6
Paul <pavel.glugxix@gmail.com>
2026-01-09 12:42:38 +0300

    refactor(constants)!: consolidate throw message constants

--- src/modules/core/constants/throws-msg.constant.ts
+++ src/modules/core/constants/throws-msg.constant.ts
@@ -1,12 +1,8 @@
 import { appConstants } from '@core/constants/app.constants';
 
-export const STRICT_STORE_THROWS_MESSAGES = {
+export const THROWS_MSG_CONSTANTS = {
   merge: {
     notInitialized: `${appConstants.APP_NAME}.merge: Cannot initialize the object. Use ${appConstants.APP_NAME}.save for initial value`,
     targetNotPlainObject: `${appConstants.APP_NAME}.merge: Can only merge into plain objects`,
-  },
-  key: {
-    containsColon: 'Namespace and name must not contain the ":" character',
-    emptyNameOrNamespace: 'The name or namespace cannot be empty',
   },
 } as const;
