c96315f43ca796deb21ac1d1cd430a1874a9e584
Paul <pavel.glugxix@gmail.com>
2026-01-09 11:52:43 +0300

    chore(strict-json): rename serializer class

--- src/index.module.ts
+++ src/index.module.ts
@@ -1,9 +1,9 @@
 import { StrictStoreService } from '@strict-store/strict-store.service';
 import { createKey } from '@strict-store/app/create-key';
 import { WebStorageProvider } from '@strict-store/infrastructure/providers/web-storage.provider';
-import { StrictJsonSerializer } from '@strict-json/strict-json.service';
+import { StrictJsonService } from '@strict-json/strict-json.service';
 
-const serializer = new StrictJsonSerializer();
+const serializer = new StrictJsonService();
 const StrictStore = new StrictStoreService(new WebStorageProvider(), serializer);
 
 export { StrictStore, createKey };
--- src/modules/strict-json/strict-json.service.ts
+++ src/modules/strict-json/strict-json.service.ts
@@ -3,10 +3,7 @@ import { StoreKey } from '@core/entities/store-key.entity';
 import { SerializerPort } from '@core/ports/serializer.port';
 import { SuperJSON } from 'superjson';
 
-/**
- * A strict JSON serializer that handles advanced types like object, array, bigint, Map, Set, and TypedArray.
- */
-export class StrictJsonSerializer implements SerializerPort {
+export class StrictJsonService implements SerializerPort {
 
   parse<T extends Persistable>(value: string): T {
     return SuperJSON.parse<T>(value);
